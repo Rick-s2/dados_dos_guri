09a403ff75ac028177de8c97d7b24547c4fa505d
Rick-s2 <114995830+Rick-s2@users.noreply.github.com>
2023-04-07 00:08:34 -0300

    Add files via upload

---
+++ pasta_tfc/SWEGA_escoteiros/scripts.js
@@ -0,0 +1,12 @@
+function abrir_nav(){
+
+    document.getElementById("menu_oculto").style.width="250px";
+    document.getElementById("principal").style.width="250px";
+
+}
+function fechar_nav(){
+
+    document.getElementById("menu_oculto").style.width="0";
+    document.getElementById("menu_oculto").style.width="0";
+
+}
